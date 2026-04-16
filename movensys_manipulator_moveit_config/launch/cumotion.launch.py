93e90d1ef29073c6c7ce65285460d98011ca55cf
mfikih <mfikih@movensys.com>
2026-04-16 14:11:10 +0900

    running cumotion

--- movensys_manipulator_moveit_config/launch/cumotion.launch.py
+++ movensys_manipulator_moveit_config/launch/cumotion.launch.py
@@ -92,6 +92,7 @@ def launch_setup(context: LaunchContext, *args, **kwargs) -> List[Node]:
                         .robot_description(file_path=os.path.join(desc_share,   'urdf',   manipulator_model, 'movensys_manipulator.xacro'))
                         .robot_description_semantic(file_path=os.path.join(config_share, 'config', manipulator_model, 'movensys_manipulator.srdf'))
                         .robot_description_kinematics(file_path=os.path.join(config_share, 'config', manipulator_model, 'kinematics.yaml'))
+                        .joint_limits(file_path=os.path.join(config_share, 'config', manipulator_model, 'joint_limits.yaml'))
                         .planning_pipelines(pipelines=['ompl'], default_planning_pipeline='ompl')
                         .trajectory_execution(file_path=os.path.join(config_share, 'config', manipulator_model, 'moveit_controllers.yaml'))
                         .to_moveit_configs())
