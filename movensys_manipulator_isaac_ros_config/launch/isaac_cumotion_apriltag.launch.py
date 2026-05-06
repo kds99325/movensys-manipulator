452dca1091fb5429561e3881fe44e299b0590079
mfikih <mfikih@movensys.com>
2026-05-06 13:30:13 +0900

    add camera hand launchy in real

--- movensys_manipulator_isaac_ros_config/launch/isaac_cumotion_apriltag.launch.py
+++ movensys_manipulator_isaac_ros_config/launch/isaac_cumotion_apriltag.launch.py
@@ -1,6 +1,7 @@
 import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
@@ -16,6 +17,14 @@ def generate_launch_description():
     )
 
     pkg = get_package_share_directory('movensys_manipulator_isaac_ros_config')
+    perception_pkg = get_package_share_directory('movensys_manipulator_perception')
+
+    camera_hand_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(perception_pkg, 'launch', 'camera_hand.launch.py')
+        ),
+        condition=UnlessCondition(use_sim_time),
+    )
 
     apriltag_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -33,6 +42,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         declare_use_sim_time,
+        camera_hand_launch,
         apriltag_launch,
         cumotion_launch,
     ])
